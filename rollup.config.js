671023dff9adc3fc0595f6d5232b92fac67a73d1
Fábio de Carvalho Almeida <falmeida@uolinc.com>
2020-04-18 17:05:55 -0300

    feat: Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/index.js',
+		output: {
+			name: 'disguise',
+			file: pkg.browser,
+			format: 'umd',
+			exports: 'named',
+		},
+		plugins: [
+			resolve(), // so Rollup can find `dependency`
+			commonjs(), // so Rollup can convert `dependency` to an ES module
+		],
+	},
+
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	{
+		input: 'src/index.js',
+		// external: ['dependency'],
+		output: [
+			{ name: 'disguise', format: 'cjs', file: pkg.main },
+			{ name: 'disguise', format: 'es', file: pkg.module },
+		],
+	},
+];
